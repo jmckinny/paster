984733a64f131af5a0018ebb36ae024da0131233
jmckinny <justinmckinney.md@gmail.com>
2022-11-01 16:23:58 -0400

    cleanup: cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod paste;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, new_paste,get_paste])
+    rocket::build().mount("/", routes![index, new_paste, get_paste])
 }
 #[get("/")]
 fn index() -> &'static str {
@@ -23,7 +23,7 @@ fn index() -> &'static str {
     "
 }
 #[get("/<id>")]
-async fn get_paste(id: u64) -> std::io::Result<String>{
+async fn get_paste(id: u64) -> std::io::Result<String> {
     let mut file = std::fs::File::open(format!("pastes/{}", id))?;
     let mut data = String::new();
     file.read_to_string(&mut data)?;
@@ -36,10 +36,7 @@ async fn new_paste(paste: Data<'_>) -> std::io::Result<String> {
     let mut hasher = DefaultHasher::new();
     text.hash(&mut hasher);
     let id = hasher.finish();
-    let item = paste::Paste::new(
-        id,
-        text
-    );
+    let item = paste::Paste::new(id, text);
     item.into_file()?;
     Ok(id.to_string())
 }
